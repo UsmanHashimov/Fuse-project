122a32c454c5ee45971799bb7dd2786c61bed177
UsmanHashimov <hashimovusman01@gmail.com>
2024-04-18 16:03:16 +0500

    :100:

---
+++ fuse-starter-18/labs-starter/src/app/modules/admin/pages/pricing/simple/simple.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { PricingSimpleComponent } from '../simple/simple.component';
+
+export default [
+    {
+        path     : '',
+        component: PricingSimpleComponent,
+    },
+] as Routes;
---
+++ fuse-starter-18/labs-starter/src/app/modules/admin/pages/pricing/single/single.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { PricingSingleComponent } from '../single/single.component';
+
+export default [
+    {
+        path     : '',
+        component: PricingSingleComponent,
+    },
+] as Routes;
